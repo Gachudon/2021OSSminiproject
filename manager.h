b4eef255abdbe978a7217750918df2072c5b9340
HurSeungDon <zzaa9999@naver.com>
2021-04-17 14:35:57 +0900

    Create manager.h

---
+++ manager.h
@@ -0,0 +1,5 @@
+int loadFruit(Fruit *f, int *count);        //File에서 데이터 불러오기
+int saveFruit(Fruit *f, int count);         //File에 저장
+void searchFruitName(Fruit *f, int count); //제품이름 검색
+void searchPrice(Fruit *f, int count);       //제품 가격 범위를 받아서 그 사이 가격인 제품 출력
+void searchWeight(Fruit *f,int count);     //제품 무게 범위를 받아서 그 사이 무게인 제품 출력
